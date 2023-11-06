aa144dc9cff09e3aab6a3688e9226f15a23cc04c
0dimen <first11@konkuk.ac.kr>
2023-11-06 15:41:02 +0900

    LogiCallback

--- src/Pages/LoginCallback.tsx
+++ src/Pages/LoginCallback.tsx
@@ -7,6 +7,21 @@ export function LoginCallback(){
     const [searchParameters, _] = useSearchParams();
     const navigate = useNavigate(); // 이동 시킬 때  쓰는 훅
     const requestToServer = async (code: string) => {
+        // Request Body
+        const requestBody = {
+            "provider": "Google",
+            "authenticationCode": code
+        }
+    
+        const response = await axios.post("https://api.blockinfra.kangdroid.me/auth/login", requestBody);
+        console.log(response);
+    
+        // 만약 응답 값이 회원가입인 경우
+        navigate("/register");
+
+        // 만약 응답 값이 로그인 인 경우
+        localStorage.setItem("accessToken", "token");
+        navigate("/home");
     }
 
     useEffect(() => {
