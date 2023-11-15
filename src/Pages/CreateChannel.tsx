e8a45a94dec1b76714bd8d7dc8c74f07f938577a
KangDroid <hyunwoo286@gmail.com>
2023-11-15 11:37:20 +0900

    [Improvement] 채널 생성 리디자인

--- src/Pages/CreateChannel.tsx
+++ src/Pages/CreateChannel.tsx
@@ -1,19 +1,19 @@
+import { Button, Card, Form, Input } from 'antd';
 import { AxiosHeaders } from 'axios';
-import { FormEvent, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { channelApi } from '../api';
 
+type CreateChannelType = {
+  channelName: string;
+  channelDescription: string;
+  channelProfileImageUrl: string | undefined;
+};
+
 export function CreateChannel() {
   const navigate = useNavigate();
-  const [channelName, setChannelName] = useState('');
-  const [channelInfo, setChannelInfo] = useState('');
-  const [channelImgUrl, setChannelImgUrl] = useState('');
 
-  const onSubmit = (event: FormEvent<HTMLFormElement>) => {
-    // Form의 기본 엑션 방지
-    event.preventDefault();
-
+  const onSubmit = (value: CreateChannelType) => {
     let accessToken: string = localStorage.getItem('accessToken') || '';
     const headers = new AxiosHeaders();
     headers.setAuthorization(`Bearer ${accessToken}`);
@@ -21,8 +21,8 @@ export function CreateChannel() {
     (async () => {
       const response = await channelApi.createChannelAsync(
         {
-          name: channelName,
-          description: channelInfo,
+          name: value.channelName,
+          description: value.channelDescription,
           imageUrl: null,
         },
         { headers: headers },
@@ -35,38 +35,60 @@ export function CreateChannel() {
   };
 
   return (
-    <>
-      <form onSubmit={onSubmit}>
-        <h1>채널 생성</h1>
-        <div>
-          <label htmlFor="channelNameInput">채널명</label>
-          <input
-            id="channelNameInput"
-            type="text"
-            value={channelName}
-            onChange={(event) => setChannelName(event.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="channelInfoInput">채널 소개</label>
-          <input
-            id="channelInfoInput"
-            type="text"
-            value={channelInfo}
-            onChange={(event) => setChannelInfo(event.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="channelImgUrlInput">채널 프로필 이미지</label>
-          <input
-            id="channelImgUrlInput"
-            type="file"
-            value={channelImgUrl}
-            onChange={(event) => setChannelImgUrl(event.target.value)}
-          />
-        </div>
-        <button type="submit">채널 생성</button>
-      </form>
-    </>
+    <div
+      style={{
+        width: '100vw',
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Card title={'채널 생성'}>
+        <Form
+          name="basic"
+          labelCol={{ span: 9 }}
+          wrapperCol={{ span: 18 }}
+          style={{ width: 600, maxWidth: 800 }}
+          onFinish={(value) => onSubmit(value)}
+          autoComplete="off"
+        >
+          <Form.Item<CreateChannelType>
+            label="채널 이름"
+            name={'channelName'}
+            rules={[{ required: true, message: '채널 이름을 입력해 주세요!' }]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item<CreateChannelType>
+            label="채널 설명"
+            name={'channelDescription'}
+            rules={[{ required: true, message: '채널 설명을 입력해 주세요!' }]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item<CreateChannelType>
+            label="채널 채널 프로필 이미지 URL"
+            name={'channelProfileImageUrl'}
+            rules={[{ required: false, message: '비밀번호를 입력해주세요!' }]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+            }}
+          >
+            <Button type="primary" htmlType="submit">
+              채널 생성
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
   );
 }
