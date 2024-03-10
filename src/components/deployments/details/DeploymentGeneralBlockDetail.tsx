c20a9b2195f668b795129d5ae71e30d8719851cc
KangDroid <hyunwoo286@gmail.com>
2024-03-11 08:06:00 +0900

    [Feature] DB배포 결과 및 연결 문자열 확인하기

--- src/components/deployments/details/DeploymentGeneralBlockDetail.tsx
+++ src/components/deployments/details/DeploymentGeneralBlockDetail.tsx
@@ -55,6 +55,7 @@ export function DeploymentGeneralBlockDetail({
           nodeData={node as DatabaseBlock}
           priceInfo={priceList}
           plugin={deploymentProjection.pluginId}
+          deploymentProjection={deploymentProjection}
         />
       );
     }
